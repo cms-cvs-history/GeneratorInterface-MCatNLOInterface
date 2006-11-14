35263898c2437da50360b2e8e75eb74db2751ffb
Fabian Stoeckli <sha1-43e75525c3e6e41805da67101e406c6d881e76cd@cern.ch>
2006-11-14 15:15:14 +0000

    zee example added

---
+++ test/ZeeAnalyzer.h
@@ -0,0 +1,44 @@
+#ifndef ZEE_ANALYZER
+#define ZEE_ANALYZER
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "CLHEP/HepMC/WeightContainer.h"
+#include "CLHEP/HepMC/GenEvent.h"
+#include "CLHEP/HepMC/GenParticle.h"
+
+#include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
+
+#include "TH1D.h"
+#include "TFile.h"
+
+//
+// class decleration
+//
+
+class ZeeAnalyzer : public edm::EDAnalyzer {
+   public:
+      explicit ZeeAnalyzer(const edm::ParameterSet&);
+      ~ZeeAnalyzer();
+
+
+   private:
+      virtual void beginJob(const edm::EventSetup&) ;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+
+      // ----------member data ---------------------------
+      
+  std::string outputFilename;
+  TH1D* weight_histo;
+  TH1D* invmass_histo;
+
+};
+
+#endif
